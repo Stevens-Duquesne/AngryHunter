513dbae9ffb31990a14a6d01ed37348c52ecc493
ArchimusW <duquesne.stevens@gmail.com>
2019-06-03 12:16:07 +0200

    Fichier de base project

---
+++ New Unity Project/Assets/Scenes/Script/InputManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    public event UnityAction Move;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move += GetAxis;
+        Move += GetRotation;
+    }
+    void GetAxis ()
+    {
+       
+    }
+    void GetRotation()
+    {
+
+    }
+}
+
