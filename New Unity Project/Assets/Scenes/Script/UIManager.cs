e9bc8243dbe2775544c70ad49ed47b074735fe9b
ArchimusW <duquesne.stevens@gmail.com>
2019-06-06 16:00:11 +0200

    Ajout MyScene UIManager.cs (play pause)

---
+++ New Unity Project/Assets/Scenes/Script/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.B))
+        {
+            Time.timeScale = 0;
+        }
+        else if(Input.GetKey(KeyCode.P))
+        {
+            Time.timeScale = 1;
+        }
+
+    }
+}
